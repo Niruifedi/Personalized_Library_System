05ae453b3906edd931baf34e4c581161d72eddd5
Niruifedi <ifediniruizuchukwu@gmail.com>
2024-05-21 16:26:26 +0100

    Feature_User_Service

---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+namespace Personalized_Library_System.Models;
+
+
+public class User
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public string? email { get; set; }
+    public string? username { get; set; }
+}
